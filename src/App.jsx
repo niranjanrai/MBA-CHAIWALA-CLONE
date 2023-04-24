93a0160bd5d3e9d6179138fee1343e16b82a88d8
Niranjan Rai <rainiranjan1998@gmail.com>
2023-04-24 14:31:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import IntroVideo from "./components/IntroVideo";
+import Section from "./components/Section";
+import FreshTopic from "./assets/academy.png";
+import FreshTopic2 from "./assets/story.png";
+import FranchiseImg from "./assets/franchise.gif";
+import TedTalksImg from "./assets/in-the-news.gif";
+import mapImg from "./assets/locations.png";
+import coursesImg from "./assets/image2.png";
+import baratImg from "./assets/image1.png";
+import albumImg from "./assets/mba-cares.gif";
+import chaiwalaImg from "./assets/image3.png";
+
+import data from "./data/data.json";
+
+// Styles
+import "./style/App.scss";
+import "./style/intro.scss";
+import "./style/section.scss";
+
+const yellow = "#fff100",
+  pink = "#ed1e79",
+  white = "#fff",
+  brown = "#6d3d0f";
+
+function App() {
+  const {
+    freshTopic,
+    freshTopic2,
+    tedTalks,
+    franchise,
+    map,
+    courses,
+    album,
+    barat,
+    chaiwala,
+  } = data;
+  return (
+    <>
+      <IntroVideo />
+
+      {/** FreshTopic */}
+      <Section
+        h3={freshTopic.heading}
+        text={freshTopic.text}
+        btnTxt={freshTopic.btn}
+        imgSrc={FreshTopic}
+        //
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
+      {/** FreshTopic2 */}
+      <Section
+        h3={freshTopic2.heading}
+        text={freshTopic2.text}
+        btnTxt={freshTopic2.btn}
+        imgSrc={FreshTopic2}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
+      {/** tEDtALK */}
+      <Section
+        h3={tedTalks.heading}
+        text={tedTalks.text}
+        btnTxt={tedTalks.btn}
+        imgSrc={TedTalksImg}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
+      {/** Franchise */}
+      <Section
+        h3={franchise.heading}
+        text={franchise.text}
+        btnTxt={franchise.btn}
+        imgSrc={FranchiseImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
+
+      {/** Map */}
+      <Section
+        h3={map.heading}
+        text={map.text}
+        btnTxt={map.btn}
+        imgSrc={mapImg}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={brown}
+        btnColor={yellow}
+        hasBtn={false}
+      />
+
+      {/** Courses */}
+      <Section
+        h3={courses.heading}
+        text={courses.text}
+        btnTxt={courses.btn}
+        imgSize={"30%"}
+        imgSrc={coursesImg}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
+      {/** album */}
+      <Section
+        h3={album.heading}
+        text={album.text}
+        btnTxt={album.btn}
+        imgSrc={albumImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
+
+      {/** barat */}
+      <Section
+        h3={barat.heading}
+        text={barat.text}
+        btnTxt={barat.btn}
+        imgSrc={baratImg}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
+      {/** chaiwala */}
+      <Section
+        h3={chaiwala.heading}
+        text={chaiwala.text}
+        btnTxt={chaiwala.btn}
+        imgSrc={chaiwalaImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Section.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Section = ({
+  h3,
+  text,
+  hasBtn = true,
+  btnTxt,
+  imgSrc,
+  imgSize = "70%",
+  backgroundColor,
+  headingColor,
+  textColor,
+  btnBgColor,
+  btnColor,
+}) => {
+  return (
+    <section className="section" style={{ backgroundColor }}>
+      <div>
+        <h3 style={{ color: headingColor }}>{h3}</h3>
+        <p style={{ color: textColor }}> {text} </p>
+
+        {hasBtn && (
+          <button style={{ color: btnColor, backgroundColor: btnBgColor }}>
+            {btnTxt}
+          </button>
+        )}
+      </div>
+      <div>
+        <img src={imgSrc} style={{ width: imgSize }} alt="ImgSrc" />
+      </div>
+    </section>
+  );
+};
+
+export default Section;
